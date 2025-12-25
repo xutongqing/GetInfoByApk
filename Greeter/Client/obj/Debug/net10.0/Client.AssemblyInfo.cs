ec7734cc8ff119a80ef94f9a2ddf0d22bfb3ec50
xutongqing <xutongqing58@126.com>
2025-12-25 22:49:43 +0800

    *

--- Greeter/Client/obj/Debug/net10.0/Client.AssemblyInfo.cs
+++ Greeter/Client/obj/Debug/net10.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09604b6c10e165a0dfb38d7b11678dab7e87863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a52a44937567eb4b112ea1e7380e581cc6b367")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
